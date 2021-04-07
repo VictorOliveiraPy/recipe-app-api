64289e6ded3087ef6145fb1eb21a975d1b103f2b
victorhugo <victorblog410@gmail.com>
2021-04-06 23:36:40 -0300

    added recipe model

--- app/core/admin.py
+++ app/core/admin.py
@@ -34,3 +34,4 @@ class UserAdmin(BaserUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
